c5435088eaa4af133e908964a819c3983945ba26
chhimi12 <chhimi101@gmail.com>
2024-04-10 15:17:42 -0600

    Implemented cooldown features

--- character.py
+++ character.py
@@ -9,16 +9,19 @@ class Character:
         self.level = level
         self.Character_type = Character_type
         self.user_name = user_name
+        self.cooldowns = {skill: 0 for skill in skills.keys()}  # create a dict with skill names and remaining cooldown
+        # for each skill , number of turns before user can a skill , intitalized to 0.
+
 
 
 class Knight(Character):
     def __init__(self):
         user_name = input("Enter your name here :")
         skills = {
-            "Heavy strike": {"damage": 4, "level_required": 1},
-            "Sword thrust": {"damage": 3, "level_required": 1},
-            "Blade of Valor:": {"damage": 6, "level_required": 1},
-            "Heavenly Judgement": {"damage": 8, "level_required": 1}}
+            "Heavy strike": {"damage": 4, "level_required": 1,"cooldown": 2},
+            "Sword thrust": {"damage": 3, "level_required": 1,"cooldown": 3},
+            "Blade of Valor:": {"damage": 6, "level_required": 1,"cooldown": 3},
+            "Heavenly Judgement": {"damage": 8, "level_required": 1,"cooldown": 4}}
 
         super().__init__(health=15, stamina=15, attack=2, defense=16, skills=skills, mana=0, level=1,
                          Character_type="Knight", user_name=user_name)
@@ -30,8 +33,32 @@ class Mage(Character):
         skills = {
             "Fireball": {"damage": 5, "cooldown": 2, "level_required": 1},
             "Ice Shard": {"damage": 4, "cooldown": 3, "level_required": 1},
-            "Glimpse of Realization: Thunderbolt": {"damage": 7, "cooldown": 4, "level_required": 1}
+            "Cry of Thunder": {"damage": 5.5, "cooldown": 3, "level_required": 1},
+            "Glimpse of Realization: Emptiness": {"damage": 7, "cooldown": 4, "level_required": 1}
+        }
+        super().__init__(health=10, stamina=10, attack=3, defense=12, skills=skills, mana=20, level=1,
+                         Character_type="Mage", user_name=user_name)
+
+
+class Murim_Martial_Artist(Character):
+    def __init__(self):
+        user_name = input("Enter your name here :")
+        skills = {
+            "Tiger Claw Strike": {"damage": 6, "cooldown": 2, "level_required": 1},
+            "Flowing River Dance ": {"damage": 2, "cooldown": 3, "level_required": 2},
+            # increase dodge-chance for next attack and counter it , reflect attack by *2
+            "Fist of the wind": {"damage": 5.5, "cooldown": 3, "level_required": 1},
+            "Dragon's Descent: ": {"damage": 7, "cooldown": 4, "level_required": 1}
         }
 
-        super().__init__(health=10, stamina=10, attack=3, defense=12, skills=skills, mana=10, level=1,
+        super().__init__(health=10, stamina=10, attack=3, defense=12, skills=skills, mana=20, level=1,
                          Character_type="Mage", user_name=user_name)
+# Need to finsh implementing:
+
+# Implement AI states - players vs AI
+
+# AI states : using heuristic/Minmax Algorithm
+# Implement cooldown - cooldown refers to number of turns required before skill can be used again
+# Implement critical hit rate,dodge chance,defense,Mana consumption
+# Implement or remove levels ? - not important right now
+
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Game:
             print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
             print(f"{player_2.user_name}'s turn")
             print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-            Attack = self.use_skill(player_2) # player two uses their skill
+            Attack = self.use_skill(player_2)  # player two uses their skill
             player_1.health -= Attack
             if player_1.health <= 0:
                 print(f"{player_1.user_name} has been slain!")
@@ -28,47 +28,73 @@ class Game:
             print(f"{player_1.user_name}'s Health:{player_1.health}")
             print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 
+            # Decrement cooldowns
+            for skill in player_1.cooldowns.keys():
+                if player_1.cooldowns[skill] > 0:
+                    player_1.cooldowns[skill] -= 1
+            for skill in player_2.cooldowns.keys():
+                if player_2.cooldowns[skill] > 0:
+                    player_2.cooldowns[skill] -= 1
+
     def choose_character(self):
-        character_types = ["Knight", "Mage"]
-        print("Select your character type:")
-        for i, character_type in enumerate(character_types, start=1):
-            print(f"{i}. {character_type}")
+        while True:
+            print(" - - - - - - - - - - - - - -- - - - - - -- - - - - - -- - - - - - - \n")
 
-        choice = int(input("Enter the number of your choice: "))
-        if choice < 1 or choice > len(character_types):
-            print("Invalid choice. Please select a valid option.")
-            return self.choose_character()  # ask user to enter again
+            character_types = ["Knight", "Mage", "Murim_Martial_Artist"]
+            print("Select your character type:")
+            for i, character_type in enumerate(character_types, start=1):
+                print(f"{i}. {character_type}")
 
-        character_type = character_types[choice - 1]
-        if character_type == "Knight":
-            print(
-                'With valor as their blade and honor as their shield, the knight embarks on a noble quest, carving their legend across the realms')
-            return Knight()  # create a knight object here
+            choice = int(input("Enter the number of your choice : "))
+            if choice < 1 or choice > len(character_types):
+                print("Invalid choice. Please select a valid option.")
+                return self.choose_character()  # ask user to enter again
 
-        if character_type == "Mage":
-            print(
-                "Casting spells with a flick of their wrist, the mage weaves intricate arcane energies to shape the very fabric of reality")
-            return Mage()  # create a knight object here
+            character_type = character_types[choice - 1]
+            if character_type == "Knight":
+                print(
+                    'With valor as their blade and honor as their shield, the knight embarks on a noble quest, carving their legend across the realms')
+                return Knight()  # create a knight object here
+
+            if character_type == "Mage":
+                print(
+                    "Casting spells with a flick of their wrist, the mage weaves intricate arcane energies to shape the very fabric of reality")
+                return Mage()  # create a knight object here
+
+            if character_type == "Murim_Martial_Artist":
+                print(
+                    "The Murim Artist wields chi with precision, sculpting combat into a seamless display of mastery.")
+                return character.Murim_Martial_Artist()  # create a knight object here
+
 
     def use_skill(self, user_character):
+        while True:
 
-        choice = int(input("Enter 1 for Basic attack and 2 to view skills: "))
-        if choice == 1:
-            print(f"You used a basic attack and caused {user_character.attack} damage")
-            return user_character.attack
-        if choice == 2:
+            choice = int(input("Enter 1 for Basic attack and 2 to view skills : "))
 
-            available_skills = {move: info for move, info in user_character.skills.items() if
-                                info[
-                                    "level_required"] <= user_character.level}  # python dict where we filter out skills by
-            print("Available skills:")
-            for i, skill in enumerate(available_skills.keys(), start=1):
-                print(f"{i}. {skill}")
+            if choice == 1:
+                print(f"You used a basic attack and caused {user_character.attack} damage")
+                return user_character.attack
+            if choice == 2:
 
-            choice = int(input("Enter the Index of the skill you want to use: "))
-            skill_name = list(available_skills.keys())[choice - 1]
-            print(f"You used {skill_name} and caused {available_skills[skill_name]['damage']} damage")
-            return available_skills[skill_name]['damage']
+                available_skills = {move: info for move, info in user_character.skills.items() if
+                                    info[
+                                        "level_required"] <= user_character.level}  # python dict where we filter out skills by level
+                print("Available skills:")
+                for i, skill in enumerate(available_skills.keys(), start=1):
+                    print(f"{i}. {skill}")
+
+                choice = int(input("Enter the Index of the skill you want to use or 0 to go back: "))
+                if choice == 0 :
+                    continue
+                skill_name = list(available_skills.keys())[choice - 1]
+                if user_character.cooldowns[skill_name] > 0:  # new line, at first its initalized to 0
+                    print(f"{skill_name} is still on cooldown for {user_character.cooldowns[skill_name]} turns.")
+                else:
+                    print(f"You used {skill_name} and caused {available_skills[skill_name]['damage']} damage")
+                    user_character.cooldowns[skill_name] = user_character.skills[skill_name][
+                        'cooldown']  # this is where we put the value of cooldown
+                    return available_skills[skill_name]['damage']
 
 
 game = Game()
